3f3569775e451beba54b031b21f94911f67f3963
alina1412 <alina1412@mail.ru>
2022-11-18 06:43:43 +0300

    +delete handler and exceptions

--- service/endpoints/data_handlers.py
+++ service/endpoints/data_handlers.py
@@ -4,6 +4,7 @@ from service.config import app
 
 from elasticsearch import AsyncElasticsearch, RequestError
 from service.db.fake_db import MAPPING_FOR_INDEX
+from elasticsearch.exceptions import NotFoundError, BadRequestError
 
 api_router = APIRouter(
     prefix="/v1",
@@ -25,6 +26,36 @@ async def create_index(
     # user_token_data=Depends(get_user_by_token)
 ):
     """"""
-    elastic_client: AsyncElasticsearch = request.app.state.elastic_client
-    app.state.elastic_client.indices.create(index="users", mappings=MAPPING_FOR_INDEX)
+    try:
+        elastic_client: AsyncElasticsearch = request.app.state.elastic_client
+        elastic_client.indices.create(index="users", mappings=MAPPING_FOR_INDEX)
+    except BadRequestError as exc:
+        if exc.error == 'resource_already_exists_exception':
+            return {'resource_already_exists_exception'}
+        else:
+            print("sometihg else")
+            assert exc == ""
     return {"success": True}
+
+
+@api_router.post(
+    "/delete-index",
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"description": "Bad request"},
+        status.HTTP_422_UNPROCESSABLE_ENTITY: {"description": "Bad request"},
+        
+    },
+)
+async def delete_index(
+    request: Request,
+    # user_input: User = Depends(),
+    # user_token_data=Depends(get_user_by_token)
+):
+    """"""
+    try:
+        elastic_client: AsyncElasticsearch = request.app.state.elastic_client
+        elastic_client.indices.delete(index="users")
+    
+        return {"success": True}
+    except NotFoundError:
+        return ("No index to delete")
