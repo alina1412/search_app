4900e46fc6f42d1e78935ebaa9c21d9b9abeaf5c
alina1412 <alina1412@mail.ru>
2022-11-20 15:21:55 +0300

    selecting logic edit

--- service/utils/logic.py
+++ service/utils/logic.py
@@ -1,7 +1,9 @@
+from fastapi import Depends
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from service.db.crud import db_delete, db_select
-from service.db.db_settings import async_database_uri
+from service.db.db_settings import async_database_uri, get_session
 from service.db.models import Documents
 
 
@@ -12,8 +14,12 @@ async def delete_doc_from_db(id: int):
         await session.commit()
 
 
-async def select_from_id_list(session, lst):
-    cursor = await db_select(
-        session, (Documents.id, Documents.text), (Documents.id.in_(lst),)
-    )
-    print(list(cursor))
+async def select_from_db_by_ids(lst, session):
+    query = select(Documents).where(Documents.id.in_(lst)).order_by(Documents.created_date).limit(20)
+    results = (await session.execute(query)).all()
+    return list(results)
+
+    # cursor = await db_select(
+    #     session, (Documents.id, Documents.text), (Documents.id.in_(lst),)
+    # )
+    # print(list(cursor))
