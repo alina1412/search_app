dc05a008fa9b9d44003e484b918c97eb3fe62545
alina1412 <alina1412@mail.ru>
2022-11-20 15:38:11 +0300

    create_index added for the first fill

--- first_fill_index.py
+++ first_fill_index.py
@@ -1,11 +1,25 @@
 import asyncio
+from elasticsearch import AsyncElasticsearch
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from service.db.db_settings import async_database_uri
 from service.db.crud import db_select
 from service.db.models import Documents
+from service.elastic.mapping import mapping_for_index, elastic_text_settings
 from service.schemas import TextInput
 from service.config import elastic_index, app
+
+
+async def create_index_for_docs():
+    try:
+        elastic_client: AsyncElasticsearch = app.state.elastic_client
+        elastic_client.indices.create(
+            index=elastic_index,
+            mappings=mapping_for_index,
+            settings=elastic_text_settings,
+        )
+    except Exception:
+        pass
 
 
 async def elastic_insert(insert_data: TextInput):
@@ -13,6 +27,8 @@ async def elastic_insert(insert_data: TextInput):
 
 
 async def fill_from_db():
+    await create_index_for_docs()
+
     engine = create_async_engine(async_database_uri(), echo=True, future=True)
     async with engine.connect() as session:
 
