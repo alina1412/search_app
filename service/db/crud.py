c5ffdd8fb44a494663c46936eafb09dc13ea435e
alina1412 <alina1412@mail.ru>
2022-11-20 10:40:02 +0300

    fix db_delete and typings

--- service/db/crud.py
+++ service/db/crud.py
@@ -1,19 +1,27 @@
+from typing import Type
+
 from sqlalchemy import delete, insert, select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from service.db.models import DeclarativeBase
 
-async def db_insert(session: AsyncSession, model, dict_data) -> None:
+
+async def db_insert(
+    session: AsyncSession, model: Type[DeclarativeBase], dict_data
+) -> None:
     query = insert(model).values(**dict_data)
     await session.execute(query)
 
 
-async def db_select(session: AsyncSession, what, condition) -> list:
+async def db_select(session: AsyncSession, what: tuple, condition: tuple) -> list:
     query = select(*what).where(*condition)
     results = (await session.execute(query)).all()
     return list(results)
 
 
-async def db_update(session: AsyncSession, what, condition, new_data) -> None:
+async def db_update(
+    session: AsyncSession, what: Type[DeclarativeBase], condition: tuple, new_data
+) -> None:
     try:
         stmt = update(*what).where(*condition).values(**new_data)
         await session.execute(stmt)
@@ -22,6 +30,8 @@ async def db_update(session: AsyncSession, what, condition, new_data) -> None:
         await session.rollback()
 
 
-async def db_delete(session: AsyncSession, what, condition) -> None:
-    query = delete(*what).where(*condition)
+async def db_delete(
+    session: AsyncSession, what: Type[DeclarativeBase], condition: tuple
+) -> None:
+    query = delete(what).where(*condition)
     await session.execute(query)
