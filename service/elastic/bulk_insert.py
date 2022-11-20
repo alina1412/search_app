fcaac312f23513e2c38c9495f59a5211b94bb2be
alina1412 <alina1412@mail.ru>
2022-11-20 18:36:21 +0300

    delete redundant for the task

--- service/elastic/bulk_insert.py
+++
@@ -1,7 +0,0 @@
-from service.config import app
-from service.schemas import UserInput
-from service.config import elastic_index
-
-async def bulk_insert(insert_data: list[UserInput]):
-    for item in insert_data:
-        app.state.elastic_client.index(index=elastic_index, document=item)
