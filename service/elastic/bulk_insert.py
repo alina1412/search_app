b0f4bd12dd4eaa0a11419c16b23689a161a99efb
alina1412 <alina1412@mail.ru>
2022-11-20 15:22:30 +0300

    taking index from config

--- service/elastic/bulk_insert.py
+++ service/elastic/bulk_insert.py
@@ -1,7 +1,7 @@
 from service.config import app
 from service.schemas import UserInput
-
+from service.config import elastic_index
 
 async def bulk_insert(insert_data: list[UserInput]):
     for item in insert_data:
-        app.state.elastic_client.index(index="users", document=item)
+        app.state.elastic_client.index(index=elastic_index, document=item)
