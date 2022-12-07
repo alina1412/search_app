672fbe933b42e5eef43877ca0e6d9a86015c90fb
alina1412 <alina1412@mail.ru>
2022-12-07 16:27:56 +0300

    mapping changed

--- service/elastic/mapping.py
+++ service/elastic/mapping.py
@@ -1,47 +1,41 @@
-# fmt: off
+#fmt: off
+
 mapping_for_index = {
-            "properties": {
-                "id": {
-                    "type": "integer",
-                    "fields": {
-                        "keyword": {
-                            "type": "keyword"
-                        }
-                    }
-                },
-                "message": {
-                    "type": "text",
-                    "analyzer": "ngram_analyzer",
-                    "search_analyzer": "standard",
-                    "fields": {
-                        "keyword": {
-                            "type": "keyword"
-                        }
-                    }
-                },
+    "properties": {
+        "id": {
+            "type": "integer",
+            "fields": {
+                "keyword": {
+                    "type": "keyword"
+                }
             }
-        }
+        },
+        "message": {
+            "type": "text",
+            "analyzer": "my_analyzer"
+        },
+    }
+}
 
 elastic_text_settings = {
     "analysis": {
-        "filter": {
-            "ngram_filter": {
-            "type": "ngram",
-            "min_gram": 4,
-            "max_gram": 20
+        "analyzer": {
+            "my_analyzer": {
+                "type": "custom",
+                "tokenizer": "my_tokenizer",
+                "filter": [
+                    "lowercase"
+                ]
             }
-      },
-      "analyzer": {
-        "ngram_analyzer": {
-          "type": "custom",
-          "tokenizer": "standard",
-          "filter": [
-            "lowercase",
-            "ngram_filter"
-          ]
+        },
+        "tokenizer": {
+            "my_tokenizer": {
+                "type": "edge_ngram",
+                "min": 3,
+                "max": 15,
+                "token_chars": ["letter", "digit"]
+            }
         }
-      }
-    },
-    "max_ngram_diff": 50
+    }
 }
-# fmt: on
+#fmt: on
